4ee31118dc28b8199d8de8dfa9955c7e74010e55
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-01-05 14:01:02 -0600

    Add Firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:applications_tracker/screens/splash.dart';
 
-import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 82, 133, 132),
@@ -11,7 +14,11 @@ var kDarkColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 82, 133, 132),
 );
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
