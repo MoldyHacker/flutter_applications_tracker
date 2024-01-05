2b0c9f4dc5d40f32c6976957eb5e911b66fcde4b
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-01-05 13:19:23 -0600

    Add Organization model class

---
+++ lib/models/organization.dart
@@ -0,0 +1,19 @@
+class Organization {
+  final String id;
+  final String name;
+  final String industry;
+  final String? location;
+  final String? email;
+  final String? website;
+  final String? phoneNumber;
+
+  Organization({
+    required this.id,
+    required this.name,
+    required this.industry,
+    this.location,
+    this.email,
+    this.website,
+    this.phoneNumber,
+  });
+}
