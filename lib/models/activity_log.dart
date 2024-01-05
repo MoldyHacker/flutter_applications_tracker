14cecccd385f73f391cebc584af2400eedbc6393
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-01-05 14:40:40 -0600

    Refactor ActivityLog model to include description field

--- lib/models/activity_log.dart
+++ lib/models/activity_log.dart
@@ -1,13 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ActivityLog {
   final String id;
   final String userId;
   final DateTime timestamp;
-  final String activity;
+  final String description;
 
   ActivityLog({
     this.id = '',
     required this.userId,
-    required this.activity,
+    required this.description,
     required this.timestamp,
   });
+
+// Factory constructor to create an ActivityLog object from a Firestore document.
+  factory ActivityLog.fromFirestore(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+    return ActivityLog(
+      id: doc.id,
+      userId: data['userId'] ?? '',
+      timestamp: (data['timestamp'] as Timestamp).toDate(),
+      description: data['description'] ?? '',
+    );
+  }
+
+  // Method to convert an ActivityLog object into a map for Firestore.
+  Map<String, dynamic> toFirestore() {
+    return {
+      'userId': userId,
+      'timestamp': Timestamp.fromDate(timestamp),
+      'description': description,
+    };
+  }
+
+
+
 }
