cd8444d3bd34754b3cfa1201d73132bfef71ebaf
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-01-06 14:28:46 -0600

    Refactor theme and color scheme in MainApp

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:applications_tracker/screens/auth.dart';
 import 'package:applications_tracker/screens/splash.dart';
 
-
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 19, 69, 134),
 );
@@ -33,8 +32,42 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Tracker!',
       themeMode: ThemeMode.system,
-      theme: ThemeData.from(colorScheme: kColorScheme),
-      darkTheme: ThemeData.from(colorScheme: kDarkColorScheme),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, snapshot) {
