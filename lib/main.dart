467445a3ca4d893cfceaf574cedec8df2d2543a3
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-01-06 12:58:17 -0600

    Imported ApplicationsScreen and replaced SplashScreen in MainApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:applications_tracker/screens/applications.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -42,7 +43,7 @@ class MainApp extends StatelessWidget {
           }
 
           if (snapshot.hasData) {
-            return const SplashScreen();
+            return const ApplicationsScreen();
           }
 
           return const AuthScreen();
