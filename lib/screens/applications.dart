cb8a7ab177091bd536fc123fa725c793145968d3
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-01-06 12:58:13 -0600

    Add applications list, authentication, and logout functionality

--- lib/screens/applications.dart
+++ lib/screens/applications.dart
@@ -1,3 +1,5 @@
+import 'package:applications_tracker/widgets/applications_list.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ApplicationsScreen extends StatelessWidget {
@@ -5,6 +7,34 @@ class ApplicationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Track\'r',
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: const Icon(
+              // Icons.account_circle_outlined,
+              Icons.logout_outlined,
+            ),
+          ),
+        ],
+      ),
+      drawer: const Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: const ApplicationsList(),
+    );
   }
 }
