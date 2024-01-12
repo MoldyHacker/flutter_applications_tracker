e0105f1a142e0c7924ccf76f40d2fb3258936f48
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-01-12 10:57:05 -0600

    Refactor timestamp field in ActivityLog model

--- lib/models/activity_log.dart
+++ lib/models/activity_log.dart
@@ -3,32 +3,28 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ActivityLog {
   final String id;
   final String userId;
-  final DateTime timestamp;
+  final Timestamp timestamp;
   final String description;
 
   ActivityLog({
     this.id = '',
     required this.userId,
     required this.description,
-    required this.timestamp,
-  });
+  }) : timestamp = Timestamp.now();
 
-// Factory constructor to create an ActivityLog object from a Firestore document.
   factory ActivityLog.fromFirestore(DocumentSnapshot doc) {
     Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return ActivityLog(
       id: doc.id,
       userId: data['userId'] ?? '',
-      timestamp: (data['timestamp'] as Timestamp).toDate(),
       description: data['description'] ?? '',
     );
   }
 
-  // Method to convert an ActivityLog object into a map for Firestore.
   Map<String, dynamic> toFirestore() {
     return {
       'userId': userId,
-      'timestamp': Timestamp.fromDate(timestamp),
+      'timestamp': Timestamp.now(),
       'description': description,
     };
   }
