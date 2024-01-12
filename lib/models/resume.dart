50cfbbb74dc1ac2553938199fb90ed370c1aa964
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-01-12 11:18:13 -0600

    Add fromFirestore and toFirestore methods to Resume class

--- lib/models/resume.dart
+++ lib/models/resume.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Resume {
   final String id;
   final DateTime dateCreated;
@@ -10,4 +12,22 @@ class Resume {
     required this.version,
     required this.fileLink,
   });
+
+    factory Resume.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot, SnapshotOptions? options) {
+    final data = snapshot.data()!;
+    return Resume(
+      id: snapshot.id,
+      dateCreated: data['dateCreated'].toDate() ?? '',
+      version: data['version'] ?? '',
+      fileLink: data['fileLink'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'dateCreated': dateCreated,
+      'version': version,
+      'fileLink': fileLink,
+    };
+  }
 }
