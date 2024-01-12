c6f898ee0d03ee79458e9fade1f441f6fe4dd4ed
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-01-12 15:07:36 -0600

    Remove unnecessary imports in splash.dart

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,5 +1,3 @@
-import 'package:applications_tracker/widgets/applications_list.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
