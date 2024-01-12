292f3d1e2c8a688f431832c1d6f1de6cb9c984e5
MoldyHacker <56985918+MoldyHacker@users.noreply.github.com>
2024-01-12 11:01:29 -0600

    Add fromFirestore and toFirestore methods to Organization class

--- lib/models/organization.dart
+++ lib/models/organization.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Organization {
   final String id;
   final String name;
@@ -16,4 +18,28 @@ class Organization {
     this.website,
     this.phoneNumber,
   });
+
+  factory Organization.fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot, SnapshotOptions? options) {
+    final data = snapshot.data()!;
+    return Organization(
+      id: snapshot.id,
+      name: data['name'] ?? '',
+      industry: data['industry'],
+      location: data['location'],
+      email: data['email'],
+      website: data['website'],
+      phoneNumber: data['phoneNumber'],
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'name': name,
+      'industry': industry ?? FieldValue.delete(),
+      'location': location ?? FieldValue.delete(),
+      'email': email ?? FieldValue.delete(),
+      'website': website ?? FieldValue.delete(),
+      'phoneNumber': phoneNumber ?? FieldValue.delete(),
+    };
+  }
 }
